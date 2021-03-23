77bb99fda0557b65e8c7b576f87eba88d34843b8
emre-ergun <emreergun@engramsoft.com>
2021-03-23 08:50:10 +0300

    Q_PROPERTY added to comport class for message

--- comport.h
+++ comport.h
@@ -10,12 +10,18 @@ class ComPort : public QObject
 public:
     explicit ComPort(QObject *parent = nullptr);
 
+    Q_PROPERTY(QString message MEMBER m_msg NOTIFY newMessage)
+
     void SendMessage(QString msg);
 
 signals:
+    void newMessage(QVariant msg);
 
 public slots:
     void MessageChanged(QString msg);
+
+private:
+    QString m_msg;
 
 };
 
