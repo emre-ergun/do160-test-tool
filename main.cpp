cf7769e942ab3b9ac692316d3f500ea80cd67ae8
emre-ergun <emreergun@engramsoft.com>
2021-03-23 08:18:54 +0300

    variables difened and connect each other

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include <comport.h>
+#include "comport.h"
+#include "tester.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
+    Tester tester;
+    ComPort comport;
+
+    //TEST: Q_PROPERTY testing
+    QObject::connect(&tester, &Tester::MessageChanged, &comport, &ComPort::MessageChanged);
+    tester.setProperty("message", "Hello world");
+
 
     qmlRegisterType<ComPort>("com.Serial.Port", 1, 0, "ComPort");
 
